62d6cd9d3684ccb8a6d400d65c2206cc09990243
Eslam Elsnousi <56192678+EslamSnousi@users.noreply.github.com>
2024-08-22 11:41:15 +0300

    Update loginPageTest.java

--- loginPageTest.java
+++ loginPageTest.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
